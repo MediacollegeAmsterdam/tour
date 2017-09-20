1ccffcec9b2634455f21d5223236f145c31cb45b
JessedeJong <jessenicodejong@gmail.com>
2017-09-20 10:21:40 +0200

    The animation between different pictures

---
+++ code/Fade (Animatie tussen foto's/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+    <link rel="stylesheet" type="text/css" href="core.css" />
+  </head>
+  <body>
+    <div class="afbeelding">
+      <img class="image animated fadeOut" src="hond1.jpg">
+    </div>
+
+    <div class="afbeelding">
+      <img class="image animated2 fadeIn" src="hond2.jpg">
+    </div>
+
+
+  </body>
+</html>
